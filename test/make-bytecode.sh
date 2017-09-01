3f7dbe2a8c50fb8c05d5c05980e56e20e780b4d0
Nathaniel Ringo <remexre@gmail.com>
2017-09-01 13:15:32 -0500

    Adds prime sieve to test targets.

--- test/make-bytecode.sh
+++ test/make-bytecode.sh
@@ -4,3 +4,4 @@ set -eu
 
 oftb compile -f anfir -o empty.anfir
 oftb compile -f anfir -o hello.anfir std/internal/examples/hello-world
+oftb compile -f anfir -o prime-sieve.anfir std/internal/examples/prime-sieve
